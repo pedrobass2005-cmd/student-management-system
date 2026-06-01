ed6121ec3c2a43c6f941c41c4525c44704b7e7f3
Pedro Henrique-desenvolvedor <pedro.bass2005@gmail.com>
2026-06-01 14:59:16 -0300

    Update print.sistema.py

--- print.sistema.py
+++ print.sistema.py
@@ -1,1 +1,1 @@
-https://docs.google.com/document/d/1tVAIfvEwtsHekD45mQtSYqg0U_GXIBkZ/edit?usp=sharing&ouid=117406479640543043196&rtpof=true&sd=true
+https://docs.google.com/document/d/1XcMT0-wl9AdafMBkSwRozJuuBRisxh-y/edit?usp=sharing&ouid=117406479640543043196&rtpof=true&sd=true
